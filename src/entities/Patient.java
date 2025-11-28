9815b3a858acda3ee10a738f44e7cc0f3c9f262b
Long-Bottom-Leaf <drcheesewizard@gmail.com>
2025-11-28 18:59:10 -0330

    Drug class finished

--- src/entities/Patient.java
+++ src/entities/Patient.java
@@ -1,5 +1,10 @@
 package entities;
 
 public class Patient {
-    
+    private int patientId;
+    private String patientFirstName;
+    private String patientLastName;
+    private int patientDOB;
+
+
 }
