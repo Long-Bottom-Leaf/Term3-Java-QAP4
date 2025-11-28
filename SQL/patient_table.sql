29534af2b610fb6f9828a0c601d5fc357fd72147
Long-Bottom-Leaf <drcheesewizard@gmail.com>
2025-11-28 19:01:45 -0330

    added create patient table

---
+++ SQL/patient_table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE patients (
+    patientid INT PRIMARY KEY,
+    firstname VARCHAR(50),
+    lastname VARCHAR(50),
+    dob VARCHAR(20)
+);
--- src/entities/Patient.java
+++ src/entities/Patient.java
@@ -6,5 +6,26 @@ public class Patient {
     private String patientLastName;
     private int patientDOB;
 
+    public Patient(int patientId, String patientFirstName, String patientLastName, int patientDOB) {
+        this.patientId = patientId;
+        this.patientFirstName = patientFirstName;
+        this.patientLastName = patientLastName;
+        this.patientDOB = patientDOB;
+    };
 
+    @Override
+    public String toString() {
+        return "Patient: " + patientId + ", " + patientFirstName + ", " + patientLastName + ", " + patientDOB;
+    };
+
+    public static Patient fromString(String line) {
+        String[] parts = line.split(", ");
+        
+        return new Patient(
+            Integer.parseInt(parts[0]),
+            parts[1],
+            parts[2],
+            Integer.parseInt(parts[3])
+        );
+    }
 }
