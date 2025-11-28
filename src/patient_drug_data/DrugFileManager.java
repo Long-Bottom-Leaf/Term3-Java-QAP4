2743805b2dd7833f1ceb911c19dec4d4765eb2bc
Long-Bottom-Leaf <drcheesewizard@gmail.com>
2025-11-28 20:06:53 -0330

    Created DrugFileManager

--- src/patient_drug_data/DrugFileManager.java
+++ src/patient_drug_data/DrugFileManager.java
@@ -1,5 +1,38 @@
 package patient_drug_data;
 
+import entities.Drug;
+import java.io.*;
+import java.util.ArrayList;
+
 public class DrugFileManager {
-    
+    private static final String FILE_NAME = "data/drugs.txt";
+
+    public static void addDrug(Drug drug) {
+        try (FileWriter writer = new FileWriter(FILE_NAME, true)) {
+            writer.write(drug.toString() + System.lineSeparator());
+            System.out.println("Drug saved to file!");
+
+        } catch (IOException error) {
+            error.printStackTrace();
+        }
+    }
+
+    public static ArrayList<Drug> loadDrugs() {
+        ArrayList<Drug> drugList = new ArrayList<>();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(FILE_NAME))) {
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                drugList.add(Drug.fromString(line));
+            }
+
+            System.out.println("Drugs loaded from file!");
+
+        } catch (IOException error) {
+            error.printStackTrace();
+        }
+
+        return drugList;
+    }
 }
